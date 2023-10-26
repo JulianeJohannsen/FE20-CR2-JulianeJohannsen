d0b47ab025bad3ceaa78285bb13b77b92fbd98f4
Juliane Johannsen <juliane.johannsen@gmx.de>
2023-10-26 18:29:39 +0200

    added status boolean for done button

--- JS/data.js
+++ JS/data.js
@@ -4,53 +4,62 @@ let tasks = `
     "image": "../IMG/shopping.jpg",
     "description": "Shop for weekly groceries at Hofer",
     "importance": 0,
-    "deadline": "12.10.2023"
+    "deadline": "12.10.2023",
+    "status": false
 }, {
     "taskName": "Cleaning",
     "image": "../IMG/clean.jpg",
     "description": "Clean apartment",
     "importance": 0,
-    "deadline": "15.10.2023"
+    "deadline": "15.10.2023",
+    "status": false
 }, {
     "taskName": "Shopping",
     "image": "../IMG/shopping.jpg",
     "description": "Shop for weekly groceries at Spar",
     "importance": 0,
-    "deadline": "15.10.2023"
+    "deadline": "15.10.2023",
+    "status": false
 },  {
     "taskName": "Shopping",
     "image": "../IMG/shopotherstuff.jpg",
     "description": "Shop a birthday present for Jane",
     "importance": 0,
-    "deadline": "13.10.2023"
+    "deadline": "13.10.2023",
+    "status": false
 }, {
     "taskName": "Find recipes",
     "image": "../IMG/recipes.jpg",
     "description": "Recipe for Jane's birthday cake",
     "importance": 0,
-    "deadline": "10.10.2023"
+    "deadline": "10.10.2023",
+    "status": false
 }, {
     "taskName": "Shopping",
     "image": "../IMG/shopping.jpg",
     "description": "Shop ingredients for Jane's cake",
     "importance": 0,
-    "deadline": "11.10.2023"
+    "deadline": "11.10.2023",
+    "status": false
 }, {
     "taskName": "Bake",
     "image": "../IMG/cake.jpg",
     "description": "Bake Jane's cake",
     "importance": 0,
-    "deadline": "12.10.2023"
+    "deadline": "12.10.2023",
+    "status": false
 }, {
     "taskName": "Proofread",
     "image": "../IMG/read.jpg",
     "description": "Proofreading for the publisher",
     "importance": 0,
-    "deadline": "01.11.2023"
+    "deadline": "01.11.2023",
+    "status": false
 }, {
     "taskName": "Organization",
     "image": "../IMG/pinboard.jpg",
     "description": "Organize Jane's birthday party",
     "importance": 0,
-    "deadline": "10.10.2023"
+    "deadline": "10.10.2023",
+    "status": false
 }]`
